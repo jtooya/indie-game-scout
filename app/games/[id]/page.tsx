e56b5a6ea27662d784c33dd452e1da09a152f116
Jeremy Too <121914292+jtooya@users.noreply.github.com>
2026-05-26 19:55:29 +0800

    Add cards to each game's unique page

--- app/games/[id]/page.tsx
+++ app/games/[id]/page.tsx
@@ -10,20 +10,43 @@ import {
 import { supabase } from "@/lib/supabase";
 import { Game } from "@/lib/types";
 import { Button } from "@/components/ui/button";
+import { Badge } from '@/components/ui/badge';
 import { notFound } from "next/navigation";
-import {AnalystStatus} from "@/lib/types";
+import { AnalystStatus } from "@/lib/types";
+import { StatusBadge } from "@/components/games/status-badge";
+import Link from "next/link"
 
 
 export default async function GamePage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     const game = await FetchGame(id);
     return (
-        <div className="grid grid-cols-3 gap-4 p-5">
+        <div className="container mx-auto p-8">
+            <img src={game.header_image_url ?? undefined} className="block mx-auto"></img>
             <div>
-                <TitleCard {...game}/>
+                <Button asChild size="lg">
+                    <Link href={"/games"}>Back</Link>
+                </Button>
             </div>
-            <div>
-                <p>Game ID: {id}</p>
+            <div className="grid grid-cols-3 gap-4 p-5">
+                <div>
+                    <TitleCard {...game}/>
+                </div>
+                <div>
+                    <SubtitleCard {...game}/>
+                </div>
+                <div>
+                    <DescriptionCard {...game}/>
+                </div>
+                <div>
+                    <GenreCard {...game}/>
+                </div>
+                <div>
+                    <AnalystCard {...game}/>
+                </div>
+                <div>
+                    <p>Game ID: {id}</p>
+                </div>
             </div>
         </div>
     )
@@ -49,23 +72,23 @@ interface TitleProps {
     publisher: string | null
 }
 
-interface subtitleProps {
+interface SubtitleProps {
     release_date: string | null
     platforms: string[] | null
-    price: number | null
+    price_cents: number | null
+    currency: string
 }
 
-interface descriptionProps {
+interface DescriptionProps {
     short_description: string | null
-    long_description: string | null
 }
 
-interface genreProps {
+interface GenreProps {
     genres: string[] | null
     tags: string[] | null
 }
 
-interface analystProps {
+interface AnalystProps {
     analyst_status: AnalystStatus
 }
 
@@ -82,3 +105,58 @@ function TitleCard({ title, developer, publisher }: TitleProps) {
         </Card>
     )
 }
+
+function SubtitleCard({ release_date, platforms, price_cents, currency }: SubtitleProps) {
+    const priceDollars = (price_cents ?? 0) / 100;
+    let release_local = "-";
+    if (release_date !== null) {
+        release_local = (new Date(release_date)).toLocaleDateString();
+    }
+
+    return (
+        <Card>
+            <CardContent>
+                <p>Release date: {release_local}</p>
+                <p>Platforms: {platforms?.join(", ")}</p>
+                <p>Price: {priceDollars} {currency} </p>
+            </CardContent>
+        </Card>
+    )
+}
+
+function DescriptionCard({ short_description }: DescriptionProps) {
+    return (
+        <Card>
+            <CardContent>
+                <p>{ short_description }</p>
+            </CardContent>
+        </Card>
+    )
+}
+
+function GenreCard({ genres, tags }: GenreProps) {
+    return (
+        <Card>
+            <CardContent>
+                <p>Genres: {genres?.map(genre =>
+                    <Badge className="m-1" variant="default" key={genre}>{genre}</Badge>
+                )}</p>
+                <p>Tags: {tags?.map(tag =>
+                    <Badge className="m-1" variant="default" key={tag}>{tag}</Badge>
+                )}</p>
+            </CardContent>
+        </Card>
+    )
+}
+
+function AnalystCard({ analyst_status }: { analyst_status: AnalystStatus }) {
+    return (
+        <Card>
+            <CardContent>
+                <p><StatusBadge analyst_status={analyst_status}  /></p>
+            </CardContent>
+        </Card>
+    )
+}
+
+
